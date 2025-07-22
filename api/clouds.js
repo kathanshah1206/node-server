7176ec22c94641f0c4a0636a5edce0e69a1010c5
kathan shah <kathanshah12062002@gmail.com>
2025-07-22 22:31:08 +0530

    5

--- api/clouds.js
+++ api/clouds.js
@@ -2,6 +2,15 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const filePath = join(process.cwd(), "datasets", "cloud_regions.geojson");
   const data = readFileSync(filePath, "utf8");
   res.status(200).json(JSON.parse(data));
