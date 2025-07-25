4e82a1dd9ac3de08d6ae99b609ea5c5837d3a9ef
kathan shah <kathanshah12062002@gmail.com>
2025-07-25 21:47:27 +0530

    4

--- api/countries.js
+++ api/countries.js
@@ -2,10 +2,19 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const filePath = join(
     process.cwd(),
     "datasets",
-    "ne_110m_admin_0_countries.geojson"
+    "modified_crypto_exchange_countries.geojson"
   );
   const data = readFileSync(filePath, "utf8");
   res.status(200).json(JSON.parse(data));
