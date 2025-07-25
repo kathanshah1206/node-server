1a43930f3ab667a6e2d9160ad10018812bbc8a77
kathan shah <kathanshah12062002@gmail.com>
2025-07-25 21:52:53 +0530

    6

---
+++ api/populated.js
@@ -0,0 +1,21 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  const filePath = join(
+    process.cwd(),
+    "datasets",
+    "ne_110m_populated_places_simple.geojson"
+  );
+  const data = readFileSync(filePath, "utf8");
+  res.status(200).json(JSON.parse(data));
+}
